e038ed08b4e14a50120a723c6f68ff8da4bb5f5b
Johan Recaman <johanstrr@gmail.com>
2024-05-27 00:30:11 -0300

    kit sqlite integration

--- blueprints/actuator.py
+++ blueprints/actuator.py
@@ -1,32 +1,37 @@
 from flask import Blueprint, render_template, redirect, request, session
+from source.models import db, Actuator
 
 actuator = Blueprint("actuator", __name__, static_folder="static", template_folder="templates")
-
-actuators = [{"id": 1, "name": "Actuator 1", "value": "0"},]
 
 @actuator.route("/actuator")
 def actuator_blueprint():
     if "user" not in session:
         return redirect("/login")
 
-    return render_template("actuator.html", actuators=actuators)
+    return render_template("actuator.html", actuators=read_actuators())
 
 @actuator.route("/create_actuator", methods=["POST"])
 def create_actuator():
-    global actuators
     name = request.form.get("name")
+    type = request.form.get("type")
     value = request.form.get("value")
-    actuator_id = len(actuators) + 1
-    new_actuator = {"id": actuator_id, "name": name, "value": value}
-    actuators.append(new_actuator)
+
+    new_actuator = Actuator(name = name, type = type, value = value)
+    db.session.add(new_actuator)
+    db.session.commit()
+
     return redirect("/actuator")
+
+@actuator.route("/read_actuators")
+def read_actuators():
+    actuators = Actuator.query.all()
+    return actuators
 
 @actuator.route("/delete_actuator/<int:actuator_id>", methods=["POST"])
 def delete_actuator(actuator_id):
-    global actuators
-    for actuator in actuators:
-        if actuator["id"] == actuator_id:
-            actuators.remove(actuator)
-            break
+    actuator = Actuator.query.filter_by(id=actuator_id).first()
+    if actuator:
+        db.session.delete(actuator)
+        db.session.commit()
     return redirect("/actuator")
 
