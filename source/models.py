a494b3c02bbeb4a4f500b366d1b513662cd1bbde
Johan Recaman <johanstrr@gmail.com>
2024-05-30 18:14:45 -0300

    log model

--- source/models.py
+++ source/models.py
@@ -19,3 +19,9 @@ class Actuator(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     value = db.Column(db.Integer, nullable=False)
+
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    value = db.Column(db.Integer, nullable=False)
+    timestamp = db.Column(db.Date, nullable=False)
