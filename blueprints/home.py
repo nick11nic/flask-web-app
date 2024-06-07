9440f2f366dddfb9dc137e248dc4e634614ebab0
Johan Recaman <johanstrr@gmail.com>
2024-06-06 21:59:09 -0300

    mqtt onmessage endpoint on webhook

--- blueprints/home.py
+++ blueprints/home.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template, session, redirect, request, Response, jsonify
 import paho.mqtt.client as mqtt
 import threading
+import requests
 from datetime import date
+from model.models import db, LogTemperature, LogHumidity, LogActuator
 from blueprints.user import is_admin, is_statistician, is_operator 
 
 home = Blueprint("home", __name__, static_folder="static", template_folder="view")
@@ -10,12 +12,17 @@ values = {"Temperature": None, "Humidity": None}
 messages = []
 
 def on_message(client, userdata, message):
+    requests.post("http://127.0.0.1:8080/message-webhook", data=message.payload.decode())
+
+
+@home.route("/message-webhook", methods=["POST"])
+def message_webhook():
     global values
 
     old_temperature = values["Temperature"]
     old_humidity = values["Humidity"]
     
-    payload = message.payload.decode()
+    payload = request.data.decode()
     if "Temperature" in payload:
         if old_temperature != payload:
             values["Temperature"] = payload
@@ -23,7 +30,9 @@ def on_message(client, userdata, message):
 
             temp = float(payload.split(":")[1])
             if temp > 50:
-               print("Temperature is too high")
+                temp_log = LogTemperature(value=temp, timestamp=date.today())
+                db.session.add(temp_log)
+                db.session.commit()
 
     elif "Humidity" in payload:
         if old_humidity != payload: 
@@ -32,7 +41,10 @@ def on_message(client, userdata, message):
 
             humidity = float(payload.split(":")[1])
             if humidity > 80:
-                print("Humidity is too high")
+                hum_log = LogHumidity(value=humidity, timestamp=date.today())
+                db.session.add(hum_log)
+                db.session.commit()
+    return render_template("home.html", values = values, messages=messages, is_admin=is_admin(), is_statistician=is_statistician(), is_operator=is_operator())
 
 client = mqtt.Client()
 client.on_message = on_message
